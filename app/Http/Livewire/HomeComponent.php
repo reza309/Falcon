79656b751d78f432ad7ddb0bf4103efa0b2009e2
reza309 <reza112sh@gmail.com>
2021-12-20 16:20:05 +0600

    sovle mobile problem

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class HomeComponent extends Component
 {
     public function render()
     {
-        return view('livewire.home-component')->layout('layouts.base');
+        $user = User::find(Auth::user()->id);
+        return view('livewire.home-component',['user'=>$user])->layout('layouts.base');
     }
 }
