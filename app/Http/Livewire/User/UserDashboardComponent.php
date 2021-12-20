09095975af25bc0ec0ddeaeffb6fa589d196bc7c
reza309 <reza112sh@gmail.com>
2021-12-20 16:33:32 +0600

    update link dahsboard

--- app/Http/Livewire/User/UserDashboardComponent.php
+++ app/Http/Livewire/User/UserDashboardComponent.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UserDashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.user.user-dashboard-component')->layout('layouts.base');
+        $user = User::find(Auth::user()->id);
+        return view('livewire.user.user-dashboard-component')->layout('layouts.base',['user'=>$user]);
     }
 }
