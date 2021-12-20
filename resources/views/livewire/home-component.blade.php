6caa3c937161490f02256e710afdfe64df4465dd
reza309 <reza112sh@gmail.com>
2021-12-20 08:46:41 +0600

    sm style update

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -248,7 +248,7 @@
                         </div>
                         <p class="text-white-50" style="text-align: justify;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         <div class="row">
-                            <div class="col-lg-6 c-col-md-8 col-lg-12">
+                            <div class="col-lg-6 c-col-md-8 col-12">
                                 <a href="#" class="btn btn-sm btn-primary d-block d-md-block btn-custom border-0">Choose your Theme</a>
                             </div>
                         </div>
