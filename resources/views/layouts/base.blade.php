57c1f7875ffe5c6ee5927c4250a1eaf45da1b7db
reza309 <reza112sh@gmail.com>
2021-12-20 16:15:32 +0600

    sovle mobile problem

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -64,7 +64,12 @@
                                 <li class="nav-item dropdown mb-lg-0 mb-3">
                                     <a href="{{route('user.dashboard')}}" class="nav-link dropdown-toggle border rounded" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style="border-color:#070317 !important; padding:4px">
                                         My Account    
+                                        
+                                        @if($user->profile->images)
+                                        <img src="{{asset('storage/app/profile')}}/{{$user->profile->images}}" alt="Profile Image " class="img img-fluid profile-img me-lg-0 me-1 p-0" style="width:32px; height:32px; border:1px solid; padding:3px">
+                                        @else
                                         <img src="{{asset('images/profile/default.png')}}" alt="User image float-start" class="img img-fluid rounded-circle" style="width:32px; height:32px; border:1px solid; padding:3px">
+                                        @endif
                                     </a>
                                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                                         <li><a class="dropdown-item" href="#">Dashboard</a></li>
