bc60c13e5ab55627acd1d5e47d4d5bc6c0248ee2
reza309 <reza112sh@gmail.com>
2021-12-20 18:52:52 +0600

    devlop the login in product

--- app/Http/Livewire/Product/ProfuctComponent.php
+++ app/Http/Livewire/Product/ProfuctComponent.php
@@ -8,6 +8,8 @@ class ProfuctComponent extends Component
 {
     public function render()
     {
+        use App\Models\User;
+        use Illuminate\Support\Facades\Auth;
         $user = User::find(Auth::user()->id);
         return view('livewire.product.profuct-component')->layout('layouts.product',['user'=>$user]);
     }
