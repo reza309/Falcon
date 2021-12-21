9c09d35a2bfb80f1352e1c32e4fa42ac0c03d678
reza309 <reza112sh@gmail.com>
2021-12-21 22:35:38 +0600

    add favicon

--- resources/views/layouts/product.blade.php
+++ resources/views/layouts/product.blade.php
@@ -6,10 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SS Home Page</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
     <!-- Bootstrap -->
     <link href="{{asset('bootstrap/css/bootstrap.css')}}" rel="stylesheet">
     <link href="{{asset('css/demo.css')}}" rel="stylesheet">
     <link href="{{asset('tools/fontawesome-free-5.12.0-web/css/all.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/flexslider.css">
     @livewireStyles
 </head>
 
