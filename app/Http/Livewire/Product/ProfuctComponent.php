9e2a8cb45c633fe35bae67552990145adae93c07
reza309 <reza112sh@gmail.com>
2021-12-22 20:44:28 +0600

    use cart

--- app/Http/Livewire/Product/ProfuctComponent.php
+++ app/Http/Livewire/Product/ProfuctComponent.php
@@ -7,12 +7,12 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Product;
 use Livewire\WithPagination;
-
+use Cart;
 class ProfuctComponent extends Component
 {
     public function store($product_id,$product_name,$product_price)
     {
-        Cart::add($product_id,$product_name,1,$product_price)->associate(model:'App\Model\Product');
+        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
         session()->flash('success_message','Item added in cart');
         return redirect()->route('Total Payable Ammount');
     }
