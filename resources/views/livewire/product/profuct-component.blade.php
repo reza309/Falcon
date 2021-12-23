67086c8591a4816ab5068de406e06c72e7cc5440
reza309 <reza112sh@gmail.com>
2021-12-23 17:07:26 +0600

    build a tarninary operator

--- resources/views/livewire/product/profuct-component.blade.php
+++ resources/views/livewire/product/profuct-component.blade.php
@@ -170,7 +170,7 @@
 <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
   <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header">
-      <strong class="me-auto {{if(Session::has('exist_message')):'bg-danger';endif}}">Falcon</strong>
+      <strong class="me-auto {{(Session::has('exist_message'))?'bg-danger':'bg-success'}}">Falcon</strong>
       <small>11 mins ago</small>
       <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
     </div>
