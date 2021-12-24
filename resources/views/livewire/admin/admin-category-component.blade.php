6674230afcbce86bdbf837a89fbdc2d81f24ed30
reza309 <reza112sh@gmail.com>
2021-12-24 14:50:34 +0600

    update style

--- resources/views/livewire/admin/admin-category-component.blade.php
+++ resources/views/livewire/admin/admin-category-component.blade.php
@@ -4,16 +4,24 @@
         <h3>Admin categories</h3>
     </div>
     <div class="pannel-body">
-            <table class="table">
+            <table class="table bg-white">
                 <tr>
                     <th>Id</th>
                     <th>Category</th>
                     <th>Slug</th>
                     <th>Action</th>
                 </tr>
+                @foreach(@categories as $category)
                 <tr>
-                    <td></td>
+                    <td>{{$category->id}}</td>
+                    <td>{{$category->name}}</td>
+                    <td>{{$category->slug}}</td>
+                    <td>
+                        <a href="#"><i class="fas fa-edit"></i></a>
+                        <a href="#"><i class="fas fa-trash"></i></a>
+                    </td>
                 </tr>
+                @endforeach
             </table>
     </div>
     </div>
